e5de4c61f3fbc59d32ff04dd7b54b78c1815b51d
ashokpandian.s <ashokpandian.s@VD-MDU-MMA-12.HCLT.CORP.HCL.IN>
2018-11-19 18:22:48 +0530

    Test - Git Hub

--- EbaoApp/src/allocator/Caller.java
+++ EbaoApp/src/allocator/Caller.java
@@ -200,9 +200,7 @@ public class Caller {
 						//webActionKeywordsTest.AutomationStepCount= CF.excelTotalRowCount(BSDTemplateFilePath,"Automation Test Case");
 						webActionKeywordsTest.dataSheetRowCount= CF.excelTotalRowCount(BSDTemplateFilePath,"DataSheet");
 						startStepRow=4;
-						//Ashok Pandian Moopanar street - Check 1
 						
-						//ishwarya- 2
 						//Test Case Level Iteration
 						Loop:
 						for(int runOrderSheetRowNo=startStepRow;runOrderSheetRowNo <= runOrderSheetRowCount; )
